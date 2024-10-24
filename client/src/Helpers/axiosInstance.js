89aa8a38583e88c4ec455c3028741839c183b335
Vimal Negi <109637181+Vimalnegi03@users.noreply.github.com>
2024-10-24 10:25:08 +0530

    Update axiosInstance.js

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL="http://localhost:5014/api/v1"
+const BASE_URL="https://lms-backend-5kd7.onrender.com/api/v1"
 const axiosInstance=axios.create()
 axiosInstance.defaults.baseURL=BASE_URL
 axiosInstance.defaults.withCredentials=true;
